b1cdeaa6f32262f63b87e70885387e7b659128c5
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-10-03 12:27:58 +0500

    last

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,9 +1,29 @@
-<template lang="">
-  <div>
-    <h1>h\elllo</h1>
+<template lang="html">
+  <div class="bg-black">
+    <div class="container mx-auto">
+      <div class="p-4 flex items-center justify-between">
+        <div class="flex items-center gap-8">
+          <button
+            class="hover:bg-gray-900 rounded-full p-3 transition-all duration-200"
+          >
+            <i class="pi pi-align-justify text-xl text-gray-200"></i>
+          </button>
+
+          <button class="flex gap-2 items-center">
+            <i class="pi pi-youtube text-red-700 text-4xl"></i>
+            <h1 class="text-white text-2xl font-semibold">YouTube</h1>
+          </button>
+        </div>
+
+        <div class="header-two"></div>
+        <div class="header-three"></div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 export default {};
 </script>
-<style lang=""></style>
+
+<style lang="css"></style>
