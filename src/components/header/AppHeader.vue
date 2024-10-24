5a5200a06d3a36d8c05e6d450aac80757a290641
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-10-24 15:08:54 +0500

    last

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -91,7 +91,7 @@ export default {
             },
           }
         );
-        console.log(response.data);
+        // console.log(response.data.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
